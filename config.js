b3fe1a0d88c2189bbb4c6a1ebfa70753b8f38692
toypianon <91355036+toypianon@users.noreply.github.com>
2025-06-09 14:03:23 +0900

    修正

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 const config = {
-  apiKey: "AIzaSyBPYIoe5kFGMk_1jTs2EykdQrlfD6dqxF8",
-  clientId: "253084564698-jpau7o87f6uujoki3s231d4g6a3p2p0c.apps.googleusercontent.com"
+ try {
+    const API_KEY = config.apiKey;
+    const CLIENT_ID = config.clientId;
+} catch (e) {
+    showErrorMessage("config.js が読み込まれていないか、内容が不正です。");
+}
+
 };
